17b901378d2a88d671cbe8ec5dac07e2e176d90a
chupss <stas.chuprinskiy@mail.ru>
2022-12-29 21:32:07 +0300

    Add models templates in recipes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    # name
+    # color
+    # slug
+
+    pass
+
+
+class Ingredient(models.Model):
+    # name
+    # measurement_unit
+
+    pass
+
+
+class Recipe(models.Model):
+    # author
+    # name
+    # image
+    # text
+    # ingredients
+    # tags
+    # cooking_time
+
+    pass
+
+
+class RecipeIngredient(models.Model):
+    # recipe
+    # ingredient
+    # amount
+
+    pass
+
+
+class ShoppingCart(models.Model):
+    # user
+    # recipe
+
+    pass
+
+
+class Favorite(models.Model):
+    # user
+    # recipe
+
+    pass
+
+
+class Subscription(models.Model):
+    # user
+    # author
+
+    pass
