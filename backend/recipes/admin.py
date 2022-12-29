d6462cd5d1040630dbb50bae5dbbdd29cc7eae5e
chupss <stas.chuprinskiy@mail.ru>
2022-12-29 20:13:21 +0300

    Register recipes models in admin zone

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,8 @@
+from django.apps import apps
 from django.contrib import admin
 
-# Register your models here.
+
+app = apps.get_app_config('recipes')
+
+for model in app.get_models():
+    admin.site.register(model)
