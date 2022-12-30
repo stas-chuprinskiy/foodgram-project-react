7128fab9be98b5a32946fc53acfe8f1724dadafe
chupss <stas.chuprinskiy@mail.ru>
2022-12-30 03:06:03 +0300

    Define recipe model in recipes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -97,13 +97,13 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'users.User'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': (
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Tag(models.Model):
@@ -29,23 +32,67 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
-    # author
-    # name
-    # image
-    # text
-    # ingredients
-    # tags
-    # cooking_time
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='publication date'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipes',
+        verbose_name='author'
+    )
+    name = models.CharField(max_length=200, verbose_name='name')
+    image = models.ImageField(upload_to='recipes/img/', verbose_name='image')
+    text = models.TextField(verbose_name='description')
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='cooking time', help_text='integer in minutes'
+    )
+    ingredient = models.ManyToManyField(
+        Ingredient, through='RecipeIngredient', verbose_name='ingredients'
+    )
+    tags = models.ForeignKey(
+        Tag, on_delete=models.SET_NULL, null=True, related_name='recipes',
+        verbose_name='tags'
+    )
 
-    pass
+    class Meta:
+        verbose_name = 'recipe'
+        verbose_name_plural = 'recipes'
+        ordering = ('-pub_date', )
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(cooking_time__gte=1), name='cooking_time_gte_1'
+            ),
+        ]
+
+    def __str__(self):
+        return self.name[:40]
 
 
 class RecipeIngredient(models.Model):
-    # recipe
-    # ingredient
-    # amount
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='ingredients',
+        verbose_name='recipe'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name='recipes',
+        verbose_name='ingredient'
+    )
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='amount', help_text='integer in measurement unit'
+    )
 
-    pass
+    class Meta:
+        verbose_name = 'recipe and ingredient'
+        verbose_name_plural = 'recipes and ingredients'
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(amount__gte=1), name='amount_gte_1'
+            ),
+        ]
+
+    def __str__(self):
+        return (
+            f'Recipe: {self.recipe.id} <---> Ingredient: {self.ingredient.id}'
+        )
 
 
 class ShoppingCart(models.Model):
