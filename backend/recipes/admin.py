ebd457c3601fc3dac99defb5ff1490732889868e
chupss <stas.chuprinskiy@mail.ru>
2023-01-08 11:53:29 +0300

    Customize admin panel

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,8 +1,65 @@
-from django.apps import apps
 from django.contrib import admin
 
+from recipes.models import (Tag, Ingredient, Recipe, RecipeIngredient,
+                            ShoppingCart, Favorite, Subscription)
 
-app = apps.get_app_config('recipes')
 
-for model in app.get_models():
-    admin.site.register(model)
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('slug', )
+    search_fields = ('slug', )
+    search_help_text = 'SLUG'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit', )
+    list_filter = ('measurement_unit', )
+    search_fields = ('name', )
+    search_help_text = 'NAME'
+    list_per_page = 50
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', )
+    list_filter = ('author', 'tags', )
+    search_fields = ('name', )
+    search_help_text = 'NAME'
+    list_per_page = 50
+
+    readonly_fields = ('favorites', )
+
+    def favorites(self, obj):
+        return obj.favorites.count()
+    favorites.short_description = 'In favorites'
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount', )
+    search_fields = ('recipe__name', 'ingredient__name', )
+    search_help_text = 'RECIPE OR INGREDIENT NAME'
+    list_per_page = 50
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe', )
+    list_filter = ('user', )
+    search_fields = ('recipe__name', )
+    search_help_text = 'RECIPE NAME'
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe', )
+    list_filter = ('user', )
+    search_fields = ('recipe__name', )
+    search_help_text = 'RECIPE NAME'
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author', )
+    list_filter = ('user', 'author', )
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,8 +1,30 @@
-from django.apps import apps
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-app = apps.get_app_config('users')
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', )
+    list_filter = ('username', 'email', )
 
-for model in app.get_models():
-    admin.site.register(model)
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'is_staff',
+                'last_login',
+                'date_joined',
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
