f7e27ef00da236adc111c39237b9f45b2f0bcfce
chupss <stas.chuprinskiy@mail.ru>
2023-01-09 14:33:29 +0300

    Add CustomUserSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,1 +1,23 @@
+from django.contrib.auth import get_user_model
+from djoser.serializers import UserSerializer
 from rest_framework import serializers
+
+User = get_user_model()
+
+
+class CustomUserSerializer(UserSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta(UserSerializer.Meta):
+        model = User
+        fields = (
+            'email', 'id', 'username', 'first_name', 'last_name',
+            'is_subscribed',
+        )
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+
+        if obj.following.filter(author=request.user):
+            return True
+        return False
