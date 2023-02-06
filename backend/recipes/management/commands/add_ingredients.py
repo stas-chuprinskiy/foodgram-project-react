6c2483b43adb67b0c22baa1da5e4da1d306e4866
chupss <stas.chuprinskiy@mail.ru>
2023-02-06 05:32:46 +0300

    Add managment command

--- backend/recipes/management/commands/add_ingredients.py
+++ backend/recipes/management/commands/add_ingredients.py
@@ -10,30 +10,24 @@ logger = logging.getLogger(__name__)
 
 DATA_PATH = BASE_DIR.parent.joinpath('data')
 
-FILES_MODELS = {
-    'ingredients.csv': Ingredient,
+DATA_FILES = {
+    'ingredients.csv': (Ingredient, ['name', 'measurement_unit']),
 }
 
 
 class Command(BaseCommand):
-    help = 'Command adds ingredients in database'
+    help = 'Adding ingredients in database.'
 
     def handle(self, *args, **options):
-        for file_name in FILES_MODELS:
+        for file_name in DATA_FILES:
             file_path = DATA_PATH.joinpath(file_name)
-            model = FILES_MODELS.get(file_name)
+            model, fieldnames = DATA_FILES[file_name]
 
-            with open(file_path, mode='r') as data_file:
-                fieldnames = ['name', 'measurement_unit']
-                reader = csv.DictReader(data_file, fieldnames=fieldnames)
-
+            with open(file_path, mode='r') as file:
+                reader = csv.DictReader(file, fieldnames=fieldnames)
                 for row in reader:
                     try:
-                        logger.info(
-                            Ingredient.objects.get_or_create(**row)
-                        )
-                    except Exception:
-                        logger.error(
-                            f'File: {file_name} | Model: {model} | Row: {row}\n',
-                            exc_info=True
-                        )
+                        instance = model.objects.get_or_create(**row)
+                        logger.info(instance)
+                    except Exception as err:
+                        logger.error(err, exc_info=True)
