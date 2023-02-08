2113b181f334248a6bfb25272f89dbec52ec9a29
chupss <stas.chuprinskiy@mail.ru>
2023-02-08 20:56:29 +0300

    Fix db constant names in settings and env-example

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -66,12 +66,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('DB_USER', default='postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', default='5432'),
+        'ENGINE': os.getenv('django.db.backends.postgresql'),
+        'NAME': os.getenv('POSTGRES_DB', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('HOST', default='127.0.0.1'),
+        'PORT': os.getenv('PORT', default='5432'),
     }
 }
 
