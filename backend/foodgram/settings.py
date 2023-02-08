dc254cdc323e6e4e27a068aa549b2544d3d5b2c9
chupss <stas.chuprinskiy@mail.ru>
2023-02-08 21:00:26 +0300

    Fix bugs

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('django.db.backends.postgresql'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
